6e1faa274dff692085725584d3ee7657b759f4e0
DannyLien <danny.lien1109@gmail.com>
2023-08-09 22:38:00 +0800

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+
+    public void hello(){
+        System.out.println("Hello world");
+    }
+
+}
+
---
+++ src/main/kotlin/Hello.kt
@@ -0,0 +1,12 @@
+fun main() {
+//    println("Hello!! Kotlin")
+    val h = Human()
+    h.hello()
+
+}
+
+class Human(){
+    fun hello(){
+        println("Hello Kotlin Human")
+    }
+}
